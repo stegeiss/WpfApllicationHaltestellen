b45bbf64bfe1cd0e0692e3b6ae85533049728c14
stegeiss <stegeiss@web.de>
2018-02-22 08:28:37 +0100

    test1

---
+++ WpfApllicationHaltestellen/GeoPunkt.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApllicationHaltestellen
+{
+    struct GeoPunkt
+    {
+        double lon;
+        double lat;
+
+        public GeoPunkt(double lon, double lat)
+        {
+            this.lon = lon;
+            this.lat = lat;
+        }
+
+        public double Lon { get => lon; set => lon = value; }
+        public double Lat { get => lat; set => lat = value; }
+
+        const double radius = 6371.0;
+        double bogen(double winkelInGrad)
+        {
+            return winkelInGrad / 180.0 * Math.PI;
+        }
+        public double X
+        {
+            get
+            {
+                return radius * Math.Cos(bogen(lat)) * Math.Sin(bogen(lon));
+            }
+        }
+
+        public double Y
+        {
+            get
+            {
+                return radius * Math.Cos(bogen(lat)) * Math.Sin(bogen(lon));
+            }
+        }
+        public double Z
+        {
+            get
+            {
+                return radius * Math.Cos(bogen(lat));
+            }
+        }
+
+        static public double BestimmeAbstand(GeoPunkt p1, GeoPunkt p2)
+        {
+            double dX = p1.X - p2.X;
+            double dY = p1.Y - p2.Y;
+            double dZ = p1.Z - p2.Z;
+            double d = Math.Sqrt(dX * dX + dY * dY + dZ * dZ);
+            return d;
+
+            //double skalarprodukt = p1.X * p2.X + p1.Y * p2.Y + p1.Z * p2.Z;
+            //return radius * Math.Acos(skalarprodukt / (radius * radius));
+        }
+    }
+}
--- WpfApllicationHaltestellen/MainWindow.xaml.cs
+++ WpfApllicationHaltestellen/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,111 @@ namespace WpfApllicationHaltestellen
     /// </summary>
     public partial class MainWindow : Window
     {
+        Haltestelle zuletztAngeklicktHaltestelle;
+        Haltestelle[] haltestellen;
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+            {
+                string dateiname = ofd.FileName;
+                string[] zeilen = File.ReadAllLines(dateiname);
+                haltestellen = new Haltestelle[zeilen.Length - 1];
+                //for (int i = 1; i < 40; i++)
+                for (int i = 1; i < zeilen.Length; i++)
+                {
+                    string[] teile = zeilen[i].Split(';');
+                    double länge = double.Parse(teile[5]);
+                    double breite = double.Parse(teile[6]);
+                    haltestellen[i - 1] = new Haltestelle(teile[3], new GeoPunkt(länge, breite));
+                }
+
+                double minLänge = haltestellen.Min(x => x.Position.Lon);
+                double maxLänge = haltestellen.Max(x => x.Position.Lon);
+                double minBreite = haltestellen.Min(x => x.Position.Lat);
+                double maxBreite = haltestellen.Max(x => x.Position.Lat);
+
+                for (int d = 0; d < 2; d++)
+                {
+                    for (int i = 0; i < haltestellen.Length; i++)
+                    {
+                        if (d == 0 && !haltestellen[i].IstHbf || d == 1 && haltestellen[i].IstHbf)
+                        {
+                            Haltestelle h = haltestellen[i];
+                            Ellipse elli = new Ellipse();
+                            elli.Width = 5.0;
+                            elli.Height = 5.0;
+                            elli.Fill = h.IstHbf ? Brushes.Red : Brushes.DarkBlue;
+                            elli.ToolTip = h.Ort;
+                            elli.Tag = h; //Mekt sich alles vom h
+                            elli.MouseDown += ellipseGeklickt;
+
+                            zeichenfläche.Children.Add(elli);
+
+                            Canvas.SetLeft(elli, zeichenfläche.ActualWidth / (maxLänge - minLänge) * (h.Position.Lon - minLänge));
+                            Canvas.SetBottom(elli, zeichenfläche.ActualHeight / (maxBreite - minBreite) * (h.Position.Lat - minBreite));
+                        }
+                    }
+                }
+                //for (int i = 0; i < haltestellen.Length; i++)
+                //{
+                //    if (haltestellen[i].IstHbf)
+                //    {
+                //        Ellipse elli = new Ellipse();
+                //        elli.Width = 5.0;
+                //        elli.Height = 5.0;
+                //        elli.Fill = Brushes.Red;
+                //        elli.ToolTip = haltestellen[i].Ort;
+
+                //        zeichenfläche.Children.Add(elli);
+
+                //        Canvas.SetLeft(elli, zeichenfläche.ActualWidth / (maxLänge - minLänge) * (haltestellen[i].Position.Lon - minLänge));
+                //        Canvas.SetBottom(elli, zeichenfläche.ActualHeight / (maxBreite - minBreite) * (haltestellen[i].Position.Lat - minBreite));
+                //    }
+                //}
+            }
+        }
+
+        private void ellipseGeklickt(object sender, MouseButtonEventArgs e)
+        {
+
+            Haltestelle soebenAngeklickteHaltestelle = (Haltestelle)((Ellipse)sender).Tag;
+            //MessageBox.Show(((Haltestelle)((Ellipse)sender).Tag).Ort);
+            if (zuletztAngeklicktHaltestelle != null)
+            {
+
+                MessageBox.Show(GeoPunkt.BestimmeAbstand(soebenAngeklickteHaltestelle.Position,zuletztAngeklicktHaltestelle.Position).ToString());
+            }
+            zuletztAngeklicktHaltestelle = soebenAngeklickteHaltestelle;
+        }
+    }
+
+    class Haltestelle
+    {
+        private string ort;
+        GeoPunkt position;
+
+        public Haltestelle(string ort, GeoPunkt position)
+        {
+            this.ort = ort;
+            this.position = position;
+        }
+
+        public string Ort { get => ort; set => ort = value; }
+        internal GeoPunkt Position { get => position; }
+
+        public bool IstHbf
+        {
+            get
+            {
+                return ort.EndsWith("Hbf");
+            }
+        }
+
     }
 }
